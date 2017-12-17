b8273a297f00fcf0d913c6750c2b8458e8977e0d
Daniel Rodriguez <maieutiquer@gmail.com>
2017-12-17 17:59:09 +0200

    integrate google analytics

--- app/js/higherOrderComponents/withTracker.js
+++ app/js/higherOrderComponents/withTracker.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const withTracker = WrappedComponent => props => {
     // eslint-disable-next-line react/prop-types
-    // window.gtag('event', 'pageview', props.location.pathname)
+    window.gtag('event', 'pageview', props.location.pathname)
 
     return <WrappedComponent {...props} />
 }
