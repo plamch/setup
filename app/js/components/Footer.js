d87eb0303f9500dddf98df048132bd7d7f8123be
Daniel Rodriguez <maieutiquer@gmail.com>
2017-12-17 17:54:06 +0200

    microdata in header and footer

--- app/js/components/Footer.js
+++ app/js/components/Footer.js
@@ -5,23 +5,32 @@ import { faPhone, faAt, faMapMarkerAlt } from '@fortawesome/fontawesome-free-sol
 export class Footer extends PureComponent {
     render () {
         return (
-            <footer className="footer container">
+            <footer className="footer container" itemscope="" itemtype="http://schema.org/Organization" itemref="logo">
                 <address className="address row">
                     <span className="col-md-3">
                         <FontAwesomeIcon icon={faPhone} />
                         &nbsp;
-                        <a href="tel:+359 877 155 302">+359 877 155 302</a>
+                        <a href="tel:+359 877 155 302" itemprop="telephone">+359 877 155 302</a>
                     </span>
                     <span className="col-md-3">
                         <FontAwesomeIcon icon={faAt} />
                         &nbsp;
-                        <a href="mailto:dani@plam.ch">dani@plam.ch</a>
+                        <a href="mailto:dani@plam.ch" itemprop="email">dani@plam.ch</a>
                     </span>
-                    <span className="col-md-6">
+                    <span
+                        className="col-md-6"
+                        itemprop="address"
+                        itemscope=""
+                        itemtype="http://schema.org/PostalAddress"
+                    >
                         <FontAwesomeIcon icon={faMapMarkerAlt} />
                         &nbsp;
                         <a href="https://goo.gl/maps/6YYVhoGmEmK2" target="_blank">
-                            bulevard "Cherni vrah" 47, Sofia
+                            <span itemprop="streetAddress">
+                                bulevard "Cherni vrah" 47
+                            </span>, <span itemprop="addressLocality">
+                                Sofia
+                            </span>
                         </a>
                     </span>
                 </address>
