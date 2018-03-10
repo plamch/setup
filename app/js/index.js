624104d9b7b8b94f0ebaf82f65fd7016c523ed7a
Daniel Rodriguez <maieutiquer@gmail.com>
2018-03-11 00:40:35 +0200

    rename folder from css to styles

--- app/js/index.js
+++ app/js/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-snapshot'
 import { AppContainer } from 'react-hot-loader'
 import { Root } from './components/Root'
-import '../css/index.scss'
+import '../styles/index.scss'
 
 const renderApp = Component => {
     render(
